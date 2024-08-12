bc7329105108afa0fdc87e55749678af5196646f
Dustin Ratcliffe <dustin.k.ratcliffe@gmail.com>
2024-08-12 08:48:36 -0500

    Added lock to prevent multiple routines from writing to the max result (might slow down process a bit) Updated print format Added print for new max Small refactor of variable names and function args

--- graveler.go
+++ graveler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -12,44 +13,47 @@ const batchSize = 100
 var items = []int{1, 2, 3, 4}
 
 func main() {
-	run(177, 1000000000)
+	run(1000000000, 231, 177)
 }
 
-func run(target, maxRolls int) {
-	fmt.Printf("Running %d rolls...\n", maxRolls)
+func run(maxSessions, maxRolls, target int) {
+	fmt.Printf("Running %d roll sessions of %d rolls with a target of %d\n", maxSessions, maxRolls, 177)
 	start := time.Now()
 
-	rolls := 0
+	sessions := 0
 	maxOnes := 0
 	defer func() {
 		fmt.Println("***** DONE *****")
 		fmt.Println("Highest Ones Roll:", maxOnes)
-		fmt.Println("Number of Roll Sessions:", rolls)
+		fmt.Println("Number of Roll Sessions:", sessions)
 		fmt.Println("Time taken:", time.Now().Sub(start))
 	}()
 
 	ch := make(chan struct{}, batchSize)
 	var wg sync.WaitGroup
-
-	for maxOnes < target && rolls < maxRolls {
+	var mux sync.Mutex
+	for maxOnes < target && sessions < maxSessions {
 		wg.Add(1)
 		ch <- struct{}{}
 		go func() {
 			defer wg.Done()
 			numbers := []int{0, 0, 0, 0}
-			for i := 0; i < 231; i++ {
+			for i := 0; i < maxRolls; i++ {
 				roll := items[rand.Intn(len(items))]
 				numbers[roll-1]++
 			}
+			mux.Lock()
 			if numbers[0] > maxOnes {
 				maxOnes = numbers[0]
+				fmt.Printf("New Max: %d Session: %d\n", maxOnes, sessions)
 			}
+			mux.Unlock()
 			<-ch
 		}()
-		rolls++
+		sessions++
 
-		if rolls%(maxRolls/100) == 0 {
-			fmt.Printf("(%s) %d/%d - %0.2f%%\n", time.Now().Sub(start), rolls, maxRolls, (float64(rolls)/float64(maxRolls))*100)
+		if sessions%(maxSessions/100) == 0 {
+			fmt.Printf("%5.1f %% %*d/%d - Max: %d (%s)\n", (float64(sessions)/float64(maxSessions))*100, len(strconv.Itoa(maxSessions)), sessions, maxSessions, maxOnes, time.Now().Sub(start))
 		}
 	}
 	wg.Wait()
